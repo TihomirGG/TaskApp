909b1dab5348e465d261e05e552e3ee3058abc77
Tihomir <tihomir.gospodinov@abv.bg>
2021-06-17 16:05:17 +0300

    Header button works

---
+++ src/components/Header.vue
@@ -0,0 +1,60 @@
+/*//
+<template>
+  <header>
+    <div class="header">
+      <h2>Task Tracker</h2>
+      <button
+        @click="onClick"
+        :class="['header__btn', showForm ? 'red' : 'green']"
+      >
+        {{ showForm ? "Close" : "Add Task" }}
+      </button>
+    </div>
+    <TaskForm :class="[showForm ? '' : 'hidden']" />
+  </header>
+</template>
+
+<script>
+import TaskForm from './TaskForm.vue'
+export default {
+  data () {
+    return { showForm: false }
+  },
+  components: { TaskForm },
+  methods: {
+    onClick () {
+      this.showForm = !this.showForm
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.header {
+  display: flex;
+  justify-content: space-between;
+  font-family: "Poppins", sans-serif;
+  &__btn {
+    align-self: center;
+    flex-basis: 25%;
+    font-weight: bold;
+    padding: 10px 10px;
+    font-family: "Poppins", sans-serif;
+    border-radius: 8px;
+    border: none;
+    color: #fff;
+  }
+  &_btn:hover {
+    cursor: pointer;
+  }
+}
+.red {
+    background: red;
+}
+.green {
+    background: #007600;
+}
+.hidden {
+  display: none;
+}
+</style>
---
+++ src/components/TaskForm.vue
@@ -0,0 +1,17 @@
+<template>
+  <form>
+    <label for="task">Task</label>
+    <input type="text" placeholder="Add task" id="task" />
+    <label for="date">Day & Time</label>
+    <input type="text" placeholder="Add date & time" id="date" />
+    <label for="reminter">Set Reminder</label>
+    <input type="checkbox" />
+    <button>Save Task</button>
+  </form>
+</template>
+<script>
+export default {}
+</script>
+
+<style>
+</style>
